f4ad5dc9d93613f3e09aeaa7b36ed6a8c1d71f88
igorl1ma <43219164+igorl1ma@users.noreply.github.com>
2021-07-28 19:10:44 -0300

    Retirados seter e ajustado retorno

--- src/main/java/br/com/challenge/itau/exception/RestExceptionHandler.java
+++ src/main/java/br/com/challenge/itau/exception/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package br.com.challenge.itau.exception;
 
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.context.MessageSource;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.List;
 
 @RestControllerAdvice
 @Log4j2
@@ -30,32 +27,31 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<Object> handlerInvalidRegexException(InvalidRegexException invalidRegexException, WebRequest webRequest) {
         var message = messageSource.getMessage("regex.exception",null, LocaleContextHolder.getLocale());
         log.error(ExceptionUtils.getRootCauseMessage(invalidRegexException));
-        List<Error> errors = List.of(new Error(1,message));
-          return handleExceptionInternal(invalidRegexException,errors,new HttpHeaders(), HttpStatus.OK,webRequest);
+        var error = new Error(1,message);
+          return handleExceptionInternal(invalidRegexException,error,new HttpHeaders(), HttpStatus.OK,webRequest);
     }
 
     @ExceptionHandler({NullPointerException.class})
     public ResponseEntity<Object> handlerNullPointerException(NullPointerException nullPointerException, WebRequest webRequest) {
         var message = messageSource.getMessage("null.pointer.exception",null, LocaleContextHolder.getLocale());
         log.error(ExceptionUtils.getRootCauseMessage(nullPointerException));
-        List<Error> errors = List.of(new Error(2,message));
-        return handleExceptionInternal(nullPointerException,errors,new HttpHeaders(), HttpStatus.OK,webRequest);
+        var error = new Error(2,message);
+        return handleExceptionInternal(nullPointerException,error,new HttpHeaders(), HttpStatus.OK,webRequest);
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handlerException(Exception exception, WebRequest webRequest) {
         var message = messageSource.getMessage("exception",null, LocaleContextHolder.getLocale());
         log.error(ExceptionUtils.getRootCauseMessage(exception));
-        List<Error> errors = List.of(new Error(3, message));
-        return handleExceptionInternal(exception,errors,new HttpHeaders(), HttpStatus.OK,webRequest);
+        var error = new Error(3, message);
+        return handleExceptionInternal(exception,error,new HttpHeaders(), HttpStatus.OK,webRequest);
     }
 
     @Getter
-    @Setter
     public static class Error {
 
-        private Integer code;
-        private String message;
+        private final Integer code;
+        private final String message;
 
         public Error(Integer code, String message) {
             this.code = code;
