651cf2a22d66358cc01c5a05039c8206650e6b81
igorl1ma <43219164+igorl1ma@users.noreply.github.com>
2021-07-28 18:57:09 -0300

    Adicionados novos testes unitários

--- src/test/java/br/com/challenge/itau/controller/ChallengeControllerTest.java
+++ src/test/java/br/com/challenge/itau/controller/ChallengeControllerTest.java
@@ -1,5 +1,7 @@
 package br.com.challenge.itau.controller;
 
+import br.com.challenge.itau.model.PasswordRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,15 +21,18 @@ class ChallengeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void validateString() throws Exception {
+    void validateStringSuccessfully() throws Exception {
 
         var request = MockMvcRequestBuilders
                 .get("/validate/password")
                 .header("Content-Type", "application/json")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content("{\"password\": \"Hel10poli$\"}");
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("AbTp9!fok")));
 
         mockMvc.perform(request)
                 .andDo(print())
@@ -36,4 +41,37 @@ class ChallengeControllerTest {
                 .andExpect(jsonPath("message").value("Password successfully validated"));
     }
 
+    @Test
+    void validateStringFailure() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("aa")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringNull() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel(null)));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(2))
+                .andExpect(jsonPath("message").value("Password field cannot be null"));
+    }
 }
