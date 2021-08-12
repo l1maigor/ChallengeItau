aa0e633d1a448a505766c139c0ca99d029a2f1a5
igorl1ma <43219164+igorl1ma@users.noreply.github.com>
2021-08-12 15:16:14 -0300

    Adicionados testes para cobertura 100%

--- src/test/java/br/com/challenge/itau/controller/ChallengeControllerTest.java
+++ src/test/java/br/com/challenge/itau/controller/ChallengeControllerTest.java
@@ -41,7 +41,7 @@ class ChallengeControllerTest {
     }
 
     @Test
-    void validateStringFailure() throws Exception {
+    void validateStringFailureNumeric() throws Exception {
 
         var request = MockMvcRequestBuilders
                 .get("/validate/password/v2")
@@ -49,6 +49,108 @@ class ChallengeControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(new PasswordRequestModel("aa")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureLowercase() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureUppercase() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1a")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureSpecialCharacters() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1aB")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureDuplicate() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1aB!1")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureSize() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1aB!")));
+
+        mockMvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("code").value(1))
+                .andExpect(jsonPath("message").value("Validation conditions are not met"));
+    }
+
+    @Test
+    void validateStringFailureBlank() throws Exception {
+
+        var request = MockMvcRequestBuilders
+                .get("/validate/password/v2")
+                .header("Content-Type", "application/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new PasswordRequestModel("1aB! ")));
 
         mockMvc.perform(request)
                 .andDo(print())
